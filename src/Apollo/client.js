23ee65df496af8a59ecb60d6ce1bc5b391a3a2fd
Angelo <anngelo0@gmail.com>
2023-02-17 02:12:25 +0200

    chore: updated apollo client implementation

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -1,11 +1,6 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
-import { HttpLink } from "apollo-link-rest";
-
-const httpLink = new HttpLink({
-  uri: "http://localhost:3301/api",
-});
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
+  uri: "http://localhost:3301/api",
   cache: new InMemoryCache(),
-  link: ApolloLink.from([httpLink]),
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
-import { client } from "./ApolloClient/client";
+import { client } from "./Apollo/client";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <></>
+      <Todo />
     </ApolloProvider>
   );
 };
